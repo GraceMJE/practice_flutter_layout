e0a6619948c209e3aae47a49ea2cdf3fa678c08f
GraceMJE <akwndms123@naver.com>
2023-10-07 00:33:41 +0900

    스크롤 가능토록 하는 widget적용(SingChildScrollView)_column버전

--- lib/main.dart
+++ lib/main.dart
@@ -18,42 +18,55 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column( //column으로 감싸면 축(axis)이 상하좌우 다 적용됨 
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row( // Row는 가로 배치, Column은 새로 배치
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center, //좌우 폭에 대해서만 적용
-          // width: double.infinity,
-          // child: Column( //column은 상하간격 조절하는 기능 따로 있기 때문에 center 적용안됨
-          //   mainAxisSize: MainAxisSize.max,
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          //   crossAxisAlignment: CrossAxisAlignment.start, //end
-            children: [
-              Container(
-                  width: 100,
-                  height: 80,
-                  color: Colors.red,
-                  child: Text('Container 1')),
-              Container(
-                  width: 100,
-                  height: 80,
-                  color: Colors.blue,
-                  child: Text('Container 2')),
-              Container(
-                  width: 100,
-                  height: 80,
-                  color: Colors.green,
-                  child: Text('Container 3')),
-            ],
+    return SingleChildScrollView( // widget으로 감싸서 변경
+      child: Column( //column으로 감싸면 축(axis)이 상하좌우 다 적용됨
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
           ),
-        Container(
-          width: 300,
-          height: 120,
-          color: Colors.grey,
-          child: Text('Container 4'),
-        )
-      ],
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 8),
+          ),
+        ],
+      ),
     );
 
     // Placeholder(
