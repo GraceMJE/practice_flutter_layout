7643be4c1fe290ee3dbc50f47c303b6d42865844
GraceMJE <akwndms123@naver.com>
2023-10-07 00:37:52 +0900

    스크롤 가능토록 하는 widget적용(SingChildScrollView)_row버전_Tab bar에 자주 사용됨

--- lib/main.dart
+++ lib/main.dart
@@ -19,51 +19,52 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView( // widget으로 감싸서 변경
-      child: Column( //column으로 감싸면 축(axis)이 상하좌우 다 적용됨
+      scrollDirection: Axis.horizontal, //child가 row면 축 설정해줘야 함 & width무한대이면 안 됨
+      child: Row( //column으로 감싸면 축(axis)이 상하좌우 다 적용됨_Tab Bar생성시 자주 사용됨
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Container(
             color: Colors.grey,
-            width: double.infinity,
+            width: 100,
             height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
           Container(
             color: Colors.grey,
-            width: double.infinity,
+            width: 100,
             height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
           Container(
             color: Colors.grey,
-            width: double.infinity,
+            width: 100,
             height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
           Container(
             color: Colors.grey,
-            width: double.infinity,
+            width: 100,
             height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
           Container(
             color: Colors.grey,
-            width: double.infinity,
+            width: 100,
             height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
           Container(
             color: Colors.grey,
-            width: double.infinity,
+            width: 100,
             height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
           Container(
             color: Colors.grey,
-            width: double.infinity,
+            width: 100,
             height: 100,
-            margin: EdgeInsets.symmetric(vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
         ],
       ),
